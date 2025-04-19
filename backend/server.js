fece71e55cc216d8be200d84032dface47854788
ravindraogg <ravi20072005@gmail.com>
2025-04-19 19:39:51 +0530

    application ready for production

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ const MONGO_URI = process.env.MONGO_URI;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"]
+    origin: "https://code-crib.netlify.app",
+    methods: ["GET", "POST","delete"]
   }
 });
 
